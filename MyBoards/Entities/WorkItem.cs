7a4c8121cd83d58376d70c56c5a09341c1184969
Pablogp102 <pablopg102@gmail.com>
2024-04-05 16:30:05 +0200

    First commit

---
+++ MyBoards/Entities/WorkItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBoards.Entities
+{
+    public class WorkItem
+    {
+        public string State { get; set; }
+        public string Area { get; set; }
+        public string IterationPath { get; set; }
+        public int Priority { get; set; }
+        //Epic
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set;}
+        //Issue
+        public decimal Efford { get; set; }
+        //Task
+        public string Activity { get; set; }
+        public decimal RemainingWork { get; set; }
+        
+        public string Type { get; set; }
+    }
+}

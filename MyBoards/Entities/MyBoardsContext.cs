17aae967f13030b6d94fa7e129dc4dd17c51a997
Pablogp102 <pablopg102@gmail.com>
2024-04-05 20:22:01 +0200

    Second Commit

---
+++ MyBoards/Entities/MyBoardsContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBoards.Entities
+{
+    public class MyBoardsContext : DbContext
+    {
+        public MyBoardsContext(DbContextOptions<MyBoardsContext> options) : base(options) 
+        {
+            
+        }
+        public DbSet<WorkItem> WorkItems { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Comment> Commensts { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+    }
+}
